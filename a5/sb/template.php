55a63dc8b2951a6b8a3652235bcd03c29122c8cd
Melissa Guzman <52518338+mguzman11@users.noreply.github.com>
2019-10-29 23:02:15 -0700

    a5
    http://www.melissascoding.com/itc240-dev/homework/a5/bc/contact1.php
    http://www.melissascoding.com/itc240-dev/homework/a5/sb/contact.php

---
+++ a5/sb/template.php
@@ -0,0 +1,86 @@
+<? include 'includes/header.php'?>
+  <!-- Page Content -->
+  <div class="container">
+
+    <!-- Heading Row -->
+    <div class="row align-items-center my-5">
+      <div class="col-lg-7">
+      </div>
+      <!-- /.col-lg-8 -->
+      <div class="col-lg-5">
+        <h1 class="font-weight-light">Clever Business Name Goes Here</h1>
+        <p>Clever content goes here</p>
+        <p>Clever content goes here</p>
+        <p>Clever content goes here</p>
+        <p>Clever content goes here</p>
+        <p>Clever content goes here</p>
+        <p>Clever content goes here</p>
+        <p>Clever content goes here</p>
+        <a class="btn btn-primary" href="#">Call to Action!</a>
+      </div>
+      <!-- /.col-md-4 -->
+    </div>
+    <!-- /.row -->
+
+    <!-- Call to Action Well -->
+    <div class="card text-white bg-secondary my-5 py-4 text-center">
+      <div class="card-body">
+        <p class="text-white m-0">A clever tagline!</p>
+      </div>
+    </div>
+
+    <!-- Content Row -->
+    <div class="row">
+      <div class="col-md-4 mb-5">
+        <div class="card h-100">
+          <div class="card-body">
+            <h2 class="card-title">Card One</h2>
+            <p class="card-text">I love this format so I am keeping it!</p>
+            <p class="card-text">I love this format so I am keeping it!</p>
+            <p class="card-text">I love this format so I am keeping it!</p>
+            <p class="card-text">I love this format so I am keeping it!</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary btn-sm">More Info</a>
+          </div>
+        </div>
+      </div>
+      <!-- /.col-md-4 -->
+      <div class="col-md-4 mb-5">
+        <div class="card h-100">
+          <div class="card-body">
+            <h2 class="card-title">Card Two</h2>
+            <p class="card-text">I love layout so I am keeping it for future use</p>
+            <p class="card-text">I love layout so I am keeping it for future use</p>
+            <p class="card-text">I love layout so I am keeping it for future use</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary btn-sm">More Info</a>
+          </div>
+        </div>
+      </div>
+      <!-- /.col-md-4 -->
+      <div class="col-md-4 mb-5">
+        <div class="card h-100">
+          <div class="card-body">
+            <h2 class="card-title">Card Three</h2>
+            <p class="card-text">Keeping this layout for future use becuase it is so great!</p>
+            <p class="card-text">Keeping this layout for future use becuase it is so great!</p>
+            <p class="card-text">Keeping this layout for future use becuase it is so great!</p>
+            <p class="card-text">Keeping this layout for future use becuase it is so great!</p>
+            <p class="card-text">Keeping this layout for future use becuase it is so great!</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary btn-sm">More Info</a>
+          </div>
+        </div>
+      </div>
+      <!-- /.col-md-4 -->
+
+    </div>
+    <!-- /.row -->
+
+  </div>
+  <!-- /.container -->
+
+ <? include 'includes/footer.php' ?>
