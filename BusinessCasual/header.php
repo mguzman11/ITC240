e5ca45128ef88c42be2f71e58d1cdd8d3d31977a
Melissa Guzman <52518338+mguzman11@users.noreply.github.com>
2019-12-09 15:57:32 -0800

    themes

---
+++ BusinessCasual/header.php
@@ -0,0 +1,69 @@
+<?php include 'functions.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title><?=$config->title?></title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?=$config->theme_virtual?>vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="<?=$config->theme_virtual?>css/business-casual-new.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <h1 class="site-heading text-center text-white d-none d-lg-block">
+      <span class="site-heading-upper text-primary mb-3"><?=$config->slogan?></span>
+      <span class="site-heading-lower"><?=$config->banner?></span>
+    </h1>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="#">Start Bootstrap</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav mx-auto">
+              <?=makeLinks($config->nav1)?>
+          <!--
+            <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="index.html">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="about.html">About</a>
+            </li>
+            <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="products.html">Products</a>
+            </li>
+            <li class="nav-item active px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="store.html">Store</a>
+            </li>
+          </ul> -->
+        </div>
+      </div>
+    </nav>
+
+    <section class="page-section cta">
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-9 mx-auto">
+              <div class="cta-inner text-center rounded">
+
+            <!-----Header ends here--->
---
+++ db_view.php
@@ -0,0 +1,39 @@
+<?php include 'includes/config.php';?>
+<?php
+
+if (isset($_GET['id']))
+{//data, show page
+    $id=(int)$_GET['id'];
+    
+}else{//redirect back to list page
+    header('Location:db_list.php');
+}
+?>
+<?php include 'includes/header.php';?>
+
+<h3>Customer Data</h3>
+<?php
+$sql = "select * from test_Customers where CustomerID=$id";
+
+$iConn = @mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) or die(myerror(__FILE__,__LINE__,mysqli_connect_error()));
+$result = mysqli_query($iConn,$sql) or die(myerror(__FILE__,__LINE__,mysqli_error($iConn)));
+if (mysqli_num_rows($result) > 0)//at least one record!
+{//show results
+	while ($row = mysqli_fetch_assoc($result))
+    {
+	   echo "<p>";
+	   echo "FirstName: <b>" . $row['FirstName'] . "</b><br />";
+	   echo "LastName: <b>" . $row['LastName'] . "</b><br />";
+	   echo "Email: <b>" . $row['Email'] . "</b><br />";
+        
+        echo '<a href="db_view.php?id=' . $row['CustomerID'] . '">See ' . $row['FirstName'] . '</a>';
+	   echo "</p>";
+    }
+}else{//no records
+	echo '<div align="center">What! No customers?  There must be a mistake!!</div>';
+}
+
+@mysqli_free_result($result); #releases web server memory
+@mysqli_close($iConn); #close connection to database
+?>
+<?php <?php get_footer?>; ?>
