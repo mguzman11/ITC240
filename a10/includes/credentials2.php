c02adab43d2e8e2c22a28641ab4f9be97255b710
Melissa Guzman <52518338+mguzman11@users.noreply.github.com>
2019-12-13 20:21:40 -0800

    Deletes credentials

--- a10/includes/credentials2.php
+++
@@ -1,8 +0,0 @@
-<?php
-//credentials.php
-
-define('DB_NAME','db_Customers');
-define('DB_USER','melissascodingad');
-define('DB_PASSWORD','Newcareer11!');
-define('DB_HOST','mysql.melissascoding.com');
-?>
